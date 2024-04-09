bc2572b3dd7e1e231fec15e47f3c17955e3ce5fc
vigmururg <vigmurug@gmail.com>
2024-04-09 08:16:29 +0530

    Update: updated status codes

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,14 +3,17 @@ import User from "../models/userModel.js";
 export const signUpController = async (req, res) => {
     try {
         const body = req.body;
+        // Check if the user with the provided email already exists
         const user = await User.findOne({email: body.email});
         if (user) {
-            res.status(400).json({message: "User already exists"});
+            return res.status(409).json({message: "User already exists"});
         }
+
+        // If the user does not exist, create a new user
         const newUser = await User.create(body);
         res.status(201).json({message: "New user created"});
     } catch (error) {
-        res.status(500).json({message: `Error occured: ${error}`})
+        res.status(400).json({message: `Error occured: ${error}`})
     }
 }
 
